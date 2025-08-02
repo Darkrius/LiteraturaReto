a60f6a5da5af20b4598d086c90dc483c48fa3290
Darkrius <Piscontesoto@gmail.com>
2025-08-02 18:09:19 -0500

    Creando el proyecto

---
+++ src/test/java/com/example/LiteraturaReto/LiteraturaRetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LiteraturaReto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraturaRetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
